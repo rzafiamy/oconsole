18b65a6308377f83b4f5c81e272787da7d06b0c4
Rija Z. <rzafiamy@infodev.ovh>
2024-09-10 15:30:10 +0200

    Remove debug info

--- config.py
+++ config.py
@@ -9,9 +9,6 @@ load_dotenv()
 OLLAMA_HOST = os.getenv('HOST')
 OLLAMA_API_KEY = os.getenv('API_KEY')
 
-print(OLLAMA_HOST)
-print(OLLAMA_API_KEY)
-
 OLLAMA_MODEL = 'llama3.1:latest'
 OLLAMA_MAX_TOKENS = 50
 OLLAMA_TEMPERATURE = 0.5
